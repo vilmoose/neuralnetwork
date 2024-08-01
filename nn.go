671dc7f3e4825bbd9f09bf2a589b307f38450482
vilmos <vilmosf11@gmail.com>
2024-08-01 11:32:12 -0400

    added mnist train, predict

--- nn.go
+++ nn.go
@@ -236,3 +236,107 @@ func load(net *Network) {
   }
   return
 }
+
+
+/*
+ * Function to train an mnist data set (essentially the hello world of neural networks)
+ * Opens a csv file, reads each record and processes each one. For each record we read we create an array that 
+ * represents the inputs and an array that reperesents the targets. Once obtained, we run the Train() function of the network  
+ * and pass it the inputs and targets.
+ * For inputs: take each pixel from the record, convert to a value between 0.0(pixel w/no value) and 1.0 (pixel w/full value)
+ * For targets: each element of the array represents the probability of the index being the target digit.
+ */
+func mnistTrain(net *Network) {
+  rand.Seed(time.Now().UTC().UnixNano())
+  t1 := time.Now()
+  /*
+   * epochs are the total number of iterations of all the training data in one cycle
+   * the more epochs you run the better trained the neural network will be BUT over training does exist
+   * and can cause "overlift" (occurs when the nn adapts too well with training data and causes the nn to perform badly with data
+   * that it doesnt recognize)
+  */
+  for epocs := 0; epochs < 5; epochs++ { 
+    testFile, _ := os.Open("mnist_dataset/mnist_train.csv")
+    r := csv.NewReader(bufio.NewReader(testFile))
+    for {
+      record, err := r.Read()
+      if err == io.EOF {
+        break
+      }
+
+      inputs := make([]float64, net.inputs)
+      for i := range inputs {
+        x, _ := strconv.ParseFloat(record[i], 64)
+        inputs[i] =  (x / 255.0 * 0.99) + 0.01
+      }
+
+      targets := make([]float64, 10)
+      for i := range targets {
+        targets[i] = 0.01
+      }
+
+      x, _ := strconv.Atoi(record[0])
+      net.Train(inputs, targets)
+    }
+    testFile.close()
+  }
+  elapse := time.Since(t1)
+  fmt.Printf("\nTime taken to train: %s\n", elapsed)
+}
+
+/*
+ * Function predicts images from the dataset provided by mnist
+*/
+func mnistPredict(net *Network) {
+  t1 := time.Now()
+  checkFile, _ := os.Open("mnist_dataset/mnist_test.csv")
+  defer checkFile.Close()
+  score := 0
+  r := csv.NewReader(bufio.NewReader(checkFile))
+  for {
+    record, err := r.Read()
+    if err == io.EOF {
+      break
+    }
+    inputs := make([]float64, net.inputs)
+    for i := range inputs {
+      if i == 0 {
+        inputs[i] = 1.0
+      }
+      x, _ := strconv.ParseFloat(record[i], 64)
+      inputs[i] = ( x / 255.0 * 0.99) + 0.01
+    }
+    outputs := net.Predict(inputs)
+    best := 0
+    highest := 0.0
+    for i := 0; i < net.outputs; i++ {
+      if outputs.At(i, 0) > highest {
+        best = i
+        highest = outputs.At(i, 0)
+      }
+    }
+    target, _ := strconv.Atoi(record[0])
+    if best == target {
+      score++
+    }
+  }
+  elapsed := time.Since(t1)
+  fmt.Printf("\nTime taken to check: %s\n", elapsed)
+  fmt.Printf("score:", score)
+}
+
+func main(){
+  net := CreateNetwork(784, 200, 10, 0.1)
+  mnist := flag.String("mnist", " ", "Either train or predict to evaluate neural network")
+  flag.Parse()
+  switch *mnist {
+  case "train":
+    mnistTrain(&net)
+    save(net)
+  case "predict":
+    load(&net)
+    mnistPredict(&net)
+  default:
+    nil
+  }
+}
